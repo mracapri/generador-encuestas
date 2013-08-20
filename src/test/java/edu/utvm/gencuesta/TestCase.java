ae2a4ba135a7a7e98d0c0ce2b3e86111b41782c8
Mario Rivera Angeles <mra_capri@msn.com>
2013-08-20 00:00:52 -0500

    Test failed

---
+++ src/test/java/edu/utvm/gencuesta/TestCase.java
@@ -0,0 +1,12 @@
+package edu.utvm.gencuesta;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TestCase {
+	@Test
+	public void caseFailed(){
+		Assert.assertTrue(false);
+	}
+}
